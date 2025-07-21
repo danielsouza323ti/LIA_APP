af2a709a1e97c3d858c27c3448204598a375366c
Marcos Daniel de Souza Cunha <marcos.cunha@parquetec.se>
2025-07-21 09:18:48 -0300

    UIG

--- lib/avatar_page.dart
+++ lib/avatar_page.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_unity_widget/flutter_unity_widget.dart';
 
-class AvatarPage extends StatelessWidget {
-  const AvatarPage({super.key});
+class AvatarPage extends StatefulWidget {
+  const AvatarPage({Key? key}) : super(key: key);
+
+  @override
+  State<AvatarPage> createState() => _AvatarPageState();
+}
+
+class _AvatarPageState extends State<AvatarPage> {
+  late UnityWidgetController _unityWidgetController;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      body: Center(
-        child: Text(
-          'Avatar',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: UnityWidget(
+        onUnityCreated: onUnityCreated,
       ),
     );
+  }
+
+  void onUnityCreated(UnityWidgetController controller) {
+    _unityWidgetController = controller;
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import path_provider_foundation
 import speech_to_text_macos
 import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
